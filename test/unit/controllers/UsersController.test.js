7fdf9ed2c6b5a4398d910b300060fa6e6927ac86
Sabbir Ahmed <sabbiirr@gmail.com>
2015-04-18 20:10:55 +0600

    Test Updates

--- test/unit/controllers/UsersController.test.js
+++ test/unit/controllers/UsersController.test.js
@@ -1,22 +1,22 @@
 var request = require('supertest');
 
-describe('WarsController', function () {
-  describe('GET /wars', function () {
+describe('UsersController', function () {
+  describe('GET /users', function () {
 
     it('should respond with json with rc 200', function (done) {
       request(sails.hooks.http.app)
-        .get('/wars')
+        .get('/users')
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
 
-    it('should have a property name opponent', function (done) {
+    it('should have a property name username', function (done) {
       request(sails.hooks.http.app)
-        .get('/wars')
+        .get('/users')
         .expect(200)
         .end(function (err, res) {
           if (err) return done(err);
-          res.body[0].should.have.property('opponent').and.be.instanceof(String);
+          res.body[0].should.have.property('username').and.be.instanceof(String);
           done();
         });
 
