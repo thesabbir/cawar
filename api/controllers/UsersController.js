96a620461fe67c88a5ab54e635026080e3e0b2f7
Sabbir Ahmed <sabbiirr@gmail.com>
2015-04-19 13:09:44 +0600

    Authorizations

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -6,6 +6,21 @@
  */
 
 module.exports = {
-	
+  create: function (req, res) {
+    //TODO: Do some validation on the input
+    if (req.body.password !== req.body.confirmPassword) {
+      return res.json(401, {err: 'Password doesn\'t match'});
+    }
+
+    User.create({email: req.body.email, password: req.body.password}).exec(function (err, user) {
+      if (err) {
+        res.json(err.status, {err: err});
+        return;
+      }
+      if (user) {
+        res.json({user: user, token: sailsTokenAuth.issueToken({sid: user.id})});
+      }
+    });
+  }
 };
 
--- api/models/Users.js
+++ api/models/Users.js
@@ -1,19 +1,24 @@
 /**
-* Users.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Users.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+var bcrypt = require('bcrypt');
 
 module.exports = {
 
   attributes: {
     username: {
-      type : 'string',
+      type: 'string',
+      required: 'true',
       unique: true
     },
     email: {
       type: 'email'
+    },
+    encryptedPassword: {
+      type: 'string'
     },
     status: 'boolean',
     rank: {
@@ -22,9 +27,36 @@ module.exports = {
       enum: ['member', 'elder', 'leader', 'co-leader', 'inactive']
     },
     warLog: {
-      collection : 'wars',
-      via : 'participants'
+      collection: 'wars',
+      via: 'participants'
+    },
+    toJSON: function () {
+      var obj = this.toObject();
+      delete obj.encryptedPassword;
+      return obj;
     }
+  },
+
+  beforeCreate : function (values, next) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if(err) return next(err);
+
+      bcrypt.hash(values.password, salt, function (err, hash) {
+        if(err) return next(err);
+
+        values.encryptedPassword = hash;
+        next();
+      })
+    })
+  },
+
+  validPassword : function (password, user, cb) {
+    bcrypt.compare(password, user.encryptedPassword, function (err, match) {
+
+      if(err) cb(err);
+      if(match) cb(null, true);
+
+    })
   }
 };
 
